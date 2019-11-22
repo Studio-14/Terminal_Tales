c05e3719bc53fa7fcd3a003ddf3868014875cbaf
Jayson Cauble <caublejayson@gmail.com>
2019-11-22 15:14:05 -0500

    Killing semicolons is possible

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    //The health of the enemy variable
+    public int health = 100;
+
+    //Checks if the enemy is dead. If so, destroy the game object.
+    private void Update()
+    {
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    //Take damage function
+    public void takeDamage(int damageToTake)
+    {
+        health -= damageToTake;
+    }
+
+
+}
---
+++ Assets/Scripts/Semicolon.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Semicolon : MonoBehaviour
+{
+
+    //If the player jumps on top of the semicolon, kill the semicolon.
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            if (other.gameObject.transform.position.y >= gameObject.GetComponent<BoxCollider2D>().size.y)
+            {
+                GetComponent<Enemy>().takeDamage(100);
+            }
+        }
+    }
+}
