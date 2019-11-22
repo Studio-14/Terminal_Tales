7664851f909c3aafe983823a1b386218bfa2bcd5
Andrew Blondo <blondoa2@mailbox.winthrop.edu>
2019-11-21 23:03:38 -0800

    Added hello world script and empty object to hold it.

---
+++ Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    private int count;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        count = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            count++;
+            Debug.Log("Hello world #" + count);
+        }
+    }
+}
