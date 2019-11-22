d60d21d43efd9e8cc6866e86eb1e2632a7e66b96
Andrew Blondo <blondoa2@mailbox.winthrop.edu>
2019-11-21 23:18:23 -0800

    Updated HelloWorld.cs to use Unity Input System.

--- Assets/Scripts/HelloWorld.cs
+++ Assets/Scripts/HelloWorld.cs
@@ -5,20 +5,29 @@ using UnityEngine;
 public class HelloWorld : MonoBehaviour
 {
     private int count;
+    private bool buttonPressed;
     
     // Start is called before the first frame update
     void Start()
     {
         count = 0;
+        buttonPressed = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        float jumpInput = Input.GetAxisRaw("Jump");
+
+        if ((jumpInput >= 1) && (!buttonPressed))
         {
+            buttonPressed = true;
             count++;
             Debug.Log("Hello world #" + count);
+        }
+        else if (buttonPressed && (jumpInput < 1))
+        {
+            buttonPressed = false;
         }
     }
 }
