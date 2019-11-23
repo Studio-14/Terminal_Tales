9d882b0a47828d13081f497eec941a1b2dc78de9
Jayson Cauble <caublejayson@gmail.com>
2019-11-22 19:50:49 -0500

    Started work on title screen

---
+++ Assets/Scripts/Title.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Title : MonoBehaviour
+{
+    private string title = "";
+    private Text text;
+
+    private void Start()
+    {
+        text = GetComponent<Text>();
+    }
+
+    public void addCharacter(string character)
+    {
+        title += character;
+        text.text = title;
+    }
+}
