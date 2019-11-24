af3b04e47f721154f6381cbf08167157f50e3fb1
Jayson Cauble <caublejayson@gmail.com>
2019-11-23 23:08:31 -0500

    Added pause menu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    private CanvasGroup canvasGroup;
+    private bool isPaused;
+
+    private void Start()
+    {
+        //Gets the canvas group component.
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //If the player presses escape, the game pauses or resumes
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Pause();
+        }
+    }
+
+    public void Pause()
+    {
+        //If the alpha of the group is 0 (or disabled), enable the pause menu and disable gameplay.
+        if (canvasGroup.alpha <= 0)
+        {
+            canvasGroup.alpha = 1;
+            canvasGroup.interactable = true;
+            isPaused = true;
+            AudioListener.volume = 0;
+        }
+        //If the alpha of the group is 1 (or enabled), disable the pause menu and reenable gameplay.
+        else if (canvasGroup.alpha >= 1)
+        {
+            canvasGroup.alpha = 0;
+            canvasGroup.interactable = false;
+            isPaused = false;
+            AudioListener.volume = 1;
+        }
+
+        //If the game is paused, disable time.
+        if (isPaused)
+        {
+            Time.timeScale = 0;
+        }
+        //If the game is not paused, keep time running.
+        else if (!isPaused)
+        {
+            Time.timeScale = 1;
+        }
+    }
+
+    //If the player wishes to go back to the main menu, reenable time and go to the main menu scene.
+    public void QuitToMenu()
+    {
+        Time.timeScale = 1;
+        isPaused = false;
+        SceneManager.LoadScene("0_Title");
+    }
+
+    //If the player wishes to quit the game, force quit the game.
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
