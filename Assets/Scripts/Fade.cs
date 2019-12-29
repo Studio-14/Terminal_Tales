aa1d6cda10d7dfbed98095ffd9edbdcd77e3d022
Jayson Cauble <caublejayson@gmail.com>
2019-12-28 22:45:32 -0500

    Intro cutscene is now functional

---
+++ Assets/Scripts/Fade.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Fade : MonoBehaviour
+{
+
+    //Loads next scene. Triggers by animation.
+    void LoadNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
