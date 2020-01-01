391418d8354cedb11a76a22a08189250fbbbd2c0
Jayson Cauble <caublejayson@gmail.com>
2020-01-01 16:02:58 -0500

    Prevents green key from respawning

--- Assets/Scripts/Teleport.cs
+++ Assets/Scripts/Teleport.cs
@@ -10,18 +10,26 @@ public class Teleport : MonoBehaviour
     [SerializeField] float teleportY = 0;
     [SerializeField] float teleportZ = -1;
 
+    private bool hasTeleportedBefore;
+
     //teleport the player
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
             other.transform.position = new Vector3(teleportX, teleportY, teleportZ);
-            
-            //Drop items if there are any
-            DropItem[] di = GetComponents<DropItem>();
-            for (int i = 0; i < di.Length; i++)
+
+            //Checks if the player has teleported before to prevent the spawning of a key again.
+            if (!hasTeleportedBefore)
             {
-                di[i].Drop();
+                //Drop items if there are any
+                DropItem[] di = GetComponents<DropItem>();
+                for (int i = 0; i < di.Length; i++)
+                {
+                    di[i].Drop();
+                }
+
+                hasTeleportedBefore = true;
             }
         }
     }
