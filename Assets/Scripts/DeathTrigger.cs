50efd62c510da011717e802eebf29b1aa9146b0b
Jayson Cauble <caublejayson@gmail.com>
2020-01-01 21:52:38 -0500

    Tutorial improvements

--- Assets/Scripts/DeathTrigger.cs
+++ Assets/Scripts/DeathTrigger.cs
@@ -6,22 +6,13 @@ using UnityEngine.SceneManagement;
 
 public class DeathTrigger : MonoBehaviour
 {
-
-    public bool isTutorial;
     //If the player enters the death trigger, it takes an entire life.
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            if (!isTutorial)
-            {
                 PlayerPrefsManager.setHealth(0);
                 other.gameObject.GetComponent<Player>().Lives();
-            }
-            else
-            {
-                other.gameObject.transform.position = new Vector3(18, 0, 0);
-            }
         }
     }
 }
