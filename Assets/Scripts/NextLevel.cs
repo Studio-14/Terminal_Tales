e90ea817c3b63888fcf302c668d6767cb27fdc86
Jayson Cauble <caublejayson@gmail.com>
2020-01-03 22:38:36 -0500

    Removes manual boolean for NextLevel and automates setting PlayerPrefs

--- Assets/Scripts/NextLevel.cs
+++ Assets/Scripts/NextLevel.cs
@@ -6,18 +6,16 @@ using UnityEngine.SceneManagement;
 
 public class NextLevel : MonoBehaviour
 {
-
-    public bool isTutorial;
-    
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            if (isTutorial)
+            //Sets the tutorial as complete if necessary.
+            if (PlayerPrefsManager.getTutorialComplete() == 0)
             {
                 PlayerPrefsManager.setTutorialComplete(1);
             }
-            
+
             //Loads the next scene in the array of scenes.
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
             //Tells the player that they are starting, which automatically finds the starting position.
