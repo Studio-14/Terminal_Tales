1e102c24d11c12b9054b6ec3b8b96564f5e5d0fe
Jayson Cauble <caublejayson@gmail.com>
2020-01-12 21:29:56 -0500

    Game Over/Win Screens have cursors by force now

--- Assets/Scripts/Retry.cs
+++ Assets/Scripts/Retry.cs
@@ -1,10 +1,16 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Retry : MonoBehaviour
 {
+    private void Start()
+    {
+        Cursor.visible = true;
+    }
+
     //When the retry button is clicked, go to the last level but with full health and back at the beginning
     public void ButtonClick()
     {
